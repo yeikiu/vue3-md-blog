2d276aeb8f091a2afeef2249005d8acd4902aab1
JQ <jqocon@gmail.com>
2019-06-24 01:04:48 -0400

    initdata

---
+++ initdata.sh
@@ -0,0 +1,14 @@
+# navigate into the data directory
+cd data
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:yeikiu/vue-base-blog.git master:data
+
+cd -
