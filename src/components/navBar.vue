3e7e294077c22b3ec4e127257ecdfc5d2377d502
unknown <joaquin.ocon.ojeda@accenture.com>
2019-11-03 09:13:23 +0000

    chore: deps updated

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,58 +1,49 @@
   <template>
-   <div>
-    <b-navbar type="dark" toggleable="sm" variant="" class="nav-bar pr-sm-5" fixed="top">
-
+  <div>
+    <b-navbar type="dark" toggleable="sm" variant class="nav-bar pr-sm-5" fixed="top">
       <b-navbar-brand href="#" @click="$router.push({name: 'home'})">v-b-blog</b-navbar-brand>
-      
+
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto mr-sm-5 pr-sm-5">
-          
           <b-nav-item-dropdown text="Section" right>
+            <b-dropdown-item @click="$router.push({path: `/`})" href="#">All</b-dropdown-item>
             <b-dropdown-item
-              @click="$router.push({path: `/`})"
-              href="#">
-              All
-            </b-dropdown-item>
-            <b-dropdown-item class="text-capitalize"
-              v-for="(section, index) in sections" :key="index"
+              class="text-capitalize"
+              v-for="(section, index) in sections"
+              :key="index"
               @click="$router.push({path: `/${section}`})"
-              href="#">
-              {{section}}
-            </b-dropdown-item>
+              href="#"
+            >{{section}}</b-dropdown-item>
           </b-nav-item-dropdown>
-
         </b-navbar-nav>
       </b-collapse>
 
-      <Ribbon class="d-none d-sm-block"
-      v-bind="ribbonOptions"
-    ></Ribbon>
-
+      <Ribbon class="d-none d-sm-block" v-bind="ribbonOptions"></Ribbon>
     </b-navbar>
- </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "nav-bar",
-    data() {
-      return {
-        sections: Object.keys(this.$store.state.postsIndex),
-        ribbonOptions: {
-          text: 'Fork me on GitHub',
-          linkUrl: 'https://github.com/yeikiu/vue-base-blog',
-          border: false
-        }
+export default {
+  name: "nav-bar",
+  data() {
+    return {
+      sections: Object.keys(this.$store.state.postsIndex),
+      ribbonOptions: {
+        text: "Fork me on GitHub",
+        linkUrl: "https://github.com/yeikiu/vue-base-blog",
+        border: false
       }
-    }
+    };
   }
+};
 </script>
 
 <style>
 /* Custom SCSS here */
 
 .github-ribbon a {
-  background-color: #404d61!important;
+  background-color: #404d61 !important;
 }
 </style>
