7acb89b2489ab185b32f337e0d6a3053a906eb0a
joaquino <joaquino@btcx.com>
2020-08-15 18:46:30 +0100

    v2.0.15

--- src/router/index.ts
+++ src/router/index.ts
@@ -22,7 +22,7 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(process.env.NODE_ENV === 'production' && process.env.DIST_PUBLIC_PATH ? `/${process.env.DIST_PUBLIC_PATH}/` : '/'),
+  history: createWebHashHistory(),
   routes
 })
 
