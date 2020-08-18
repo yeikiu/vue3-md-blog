625795b2d9483eacb56f0041847a9bb50eba8b10
JQ <jqocon@gmail.com>
2020-08-18 22:09:50 +0100

    v2.0.35

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="my-5 text-center">
-    <div class="my-5 spinner-border" role="status">
-      <span class="sr-only">Loading...</span>
-    </div>
+    <div class="my-5 spinner-border" role="status" />
   </div>
 </template>
