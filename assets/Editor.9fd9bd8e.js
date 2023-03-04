ddb0091d5d1103d36a36293c25a4028a47b94718
Posts Hook <posts_hook@github.com>
2023-03-04 04:24:38 +0000

    [CI:deploy] deploy_dist job

--- assets/Editor.9fd9bd8e.js
+++ assets/Editor.9fd9bd8e.js
@@ -1,4 +1,4 @@
-import{_ as n,c as s,r as l,o as a}from"./index.bb003c05.js";const p={data(){return{text:`# New awesome blog post title
+import{_ as n,c as s,r as l,o as a}from"./index.7ec21041.js";const p={data(){return{text:`# New awesome blog post title
 #### February 10, 2020
 
 Beginning of your new awesome blog post...`}}};function c(i,e,d,m,o,u){const t=l("v-md-editor");return a(),s(t,{modelValue:o.text,"onUpdate:modelValue":e[0]||(e[0]=r=>o.text=r),height:"76vh","right-toolbar":"preview sync-scroll fullscreen"},null,8,["modelValue"])}var f=n(p,[["render",c]]);export{f as default};
